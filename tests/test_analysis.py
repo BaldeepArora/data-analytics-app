76d986a79ba7c5a83066227a12f2335cb9b07046
BaldeepArora <baldeep.arora111@gmail.com>
2024-11-23 00:17:37 -0500

    Updated test code

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Add the project root to PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
+
+
 from src.analysis import run_analysis
 
 def test_run_analysis():
